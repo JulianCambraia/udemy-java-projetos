0407319d5d7a6ccd6f00de90dd12fd50d8e9387c
JulianFernando <juliancambraia@gmail.com>
2019-10-06 11:24:25 -0300

    Adicionando mensagem de Bom dia

--- src/ContratoMainPrg.java
+++ src/ContratoMainPrg.java
@@ -2,7 +2,8 @@
 public class ContratoMainPrg {
 
 	public static void main(String[] args) {
-		System.out.println("Calculo de contrato de trabalho temporário");
+		System.out.println("Calculo de contrato de trabalho temporário"); 
+		System.out.println("Bom dia!");
 	}
 
 }
