15c5c12efa7c66b0e9d57ed6c9e8f2447612c130
JulianFernando <juliancambraia@gmail.com>
2019-10-06 11:50:53 -0300

    Remove as mensagens de teste

--- src/ContratoMainPrg.java
+++ src/ContratoMainPrg.java
@@ -3,8 +3,5 @@ public class ContratoMainPrg {
 
 	public static void main(String[] args) {
 		System.out.println("Calculo de contrato de trabalho temporário"); 
-		System.out.println("Bom dia!");
-		System.out.println("Boa tarde!");
-		System.out.println("Boa noite!");
 	}
 }
