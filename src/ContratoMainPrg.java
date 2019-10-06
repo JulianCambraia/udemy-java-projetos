20a5d24af6763b596f84c74c8c46a39cd75d7102
JulianFernando <juliancambraia@gmail.com>
2019-10-06 11:34:02 -0300

    Adicionando mensagem de Boa noite

--- src/ContratoMainPrg.java
+++ src/ContratoMainPrg.java
@@ -4,7 +4,7 @@ public class ContratoMainPrg {
 	public static void main(String[] args) {
 		System.out.println("Calculo de contrato de trabalho temporário"); 
 		System.out.println("Bom dia!");
-		System.out.println("Bom tarde!");
+		System.out.println("Boa tarde!");
+		System.out.println("Boa noite!");
 	}
-
 }
